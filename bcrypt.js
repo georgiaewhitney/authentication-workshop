e5c3e7a5fe0a7c8611a8d7b87361207410371f6c
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-27 15:23:35 +0000

    bcrypt testing

--- bcrypt.js
+++ bcrypt.js
@@ -7,3 +7,22 @@ console.log(password);
 bcrypt.hash(password, 12).then((hash) => console.log(hash));
 
 console.log(password);
+// node bcrypt.js
+// hunter2
+// hunter2
+// $2a$12$Vxr1mAN1DagxrFuoDsfee.oJ5OIRJD6lEiyatP350o56pT6DhhAMW
+
+// 12 === cost
+
+bcrypt.hash(password, 12).then((hash) => {
+  bcrypt.compare("hunter2", hash).then((result) => console.log(result));
+  bcrypt.compare("incorrect", hash).then((result) => console.log(result));
+});
+
+// hunter2
+// hunter2
+// $2a$12$Lgs5kwgjJCrFisp4o8OGeu0bhIx9LuBp9yM62rScGIvo0mAsMfzem
+// true
+// false
+
+// npm install bcryptjs
